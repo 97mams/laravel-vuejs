39f5417e7d94f25366b0f1a8ac21cf6ff4a7707a
97mams <97mams@gmail.com>
2025-02-11 20:31:31 +0300

    add component Header

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,32 +1,7 @@
-<template>
-    <div class="w-72 flex flex-col m-auto">
-        <Card>
-        <CardHeader>
-            <CardTitle>
-                Counter :
-                    {{ counter }}
-            </CardTitle>
-        </CardHeader>
-        <CardContent>
-            <template v-for=" user in users">
-                <h1>{{user.name}}</h1>
-                <userList :name="user.name" :age="user.age"/>
-            </template>
-            <Button @click="counter++">Increment</Button>
-        </CardContent>
-    </Card>
-    </div>
+<template class="w-full flex flex-col gap-4 pt-8">
+    <Header />
 </template>
 
 <script setup lang="ts">
-import { Card,CardContent,CardTitle,CardHeader } from '@/components/ui/card'
-import { Button } from '@/components/ui/button'
-import { ref } from 'vue';
-import userList from '@/components/userList.vue';
-const counter = ref(0)
-
-const user = [{name:'mamisoa', age: 28}, {name:"santatra", age:29}]
-
-const users = ref(user)
-
+import Header from '@/components/Header.vue';
 </script>
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <Card>
+        <CardHeader>
+            Liste de tâches
+        </CardHeader>
+        <CardContent>
+            <div>
+                <div>
+                    <Label>Nouveau tâche</Label>
+                <Input/>
+                </div>
+               <Button>Ajouter</Button> 
+            </div>
+        </CardContent>
+    </Card>
+</template>
+
+<script setup lang="ts">
+import {Input} from './ui/input';
+import { Label } from './ui/label';
+import { Button } from './ui/button';
+import {Card, CardHeader,CardContent} from './ui/card';
+
+</script>
