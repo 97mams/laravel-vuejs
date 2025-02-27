66fc4d428fd95848fe50b47c4123006d46426468
97mams <97mams@gmail.com>
2025-02-27 15:21:11 +0300

    add remove func

--- app/Http/Controllers/api/TaskController.php
+++ app/Http/Controllers/api/TaskController.php
@@ -8,13 +8,14 @@ use App\Http\Resources\TaskCollection;
 use App\Http\Resources\TaskResource;
 use App\Models\task;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use PhpParser\Node\Stmt\Return_;
 
 class TaskController extends Controller
 {
     public function index()
     {
         return new TaskCollection(task::all());
-        // return TaskResource::collection(task::all());
     }
 
     public function store(Request $request)
@@ -23,14 +24,15 @@ class TaskController extends Controller
         return new TaskResource($task);
     }
 
-    public function completed()
-    {
-        return new TaskCollection(task::query()->where('completed', '!==', 0));
-    }
-
     public function update(Request $request, task $task)
     {
         $task->update($request->all());
         return new TaskCollection($task);
+    }
+
+    public function remove(task $task)
+    {
+        $task->delete();
+        return response()->json(["message" => "deleted"]);
     }
 }
